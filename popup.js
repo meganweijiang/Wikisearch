95b9c9c3747a52725080a23a4a548877e84b1179
Megan Weijiang <meganweijiang@utexas.edu>
2017-03-23 16:31:37 -0500

    clean up code

--- popup.js
+++ popup.js
@@ -1,9 +1,11 @@
+// Listen for submitButton click event
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("submitButton").addEventListener("click", initiateSearch);
 });
 
-function initiateSearch(){
 
+// Function to open search in new tab
+function initiateSearch(){
   var search = document.getElementById("entry").value;
   search.replace(" ", "_");
   var link = "http://en.wikipedia.org/wiki/".concat(search);
